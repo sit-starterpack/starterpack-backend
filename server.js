d07702136ef1bd6810999d8dc8d96be160fcaaee
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-06-29 00:28:33 +0700

    [Update] connection for db

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const db_service = require('./services/database');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+db_service();
 app.use('/api/status', (req, res) => {
   res.status(200).json('Backend is already good');
 });
