051f6e691192c0316e6858380b425b3040b11be8
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-06-30 02:26:37 +0700

    [Add] user service

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const db_service = require('./services/database-service');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+const userRoute = require('./routers/user-route');
 require('dotenv').config();
 
 db_service();
 app.use('/api/status', (req, res) => {
   res.status(200).json('Backend is already good');
 });
-
+app.use('/api', userRoute);
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
 });
---
+++ services/user-service.js
@@ -0,0 +1,26 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const { HTTPSTATUS } = require('../enumeration/httpStatus');
+module.exports.saveUser = async (payload) => {
+  try {
+    const { std_id, name, nickname } = payload;
+    if (!name || !nickname || !std_id) {
+      throw {
+        message: 'Information invalid or empty',
+        code: HTTPSTATUS.BAD_REQUEST.code,
+      };
+    }
+    const targetUser = await User.find({ name: name });
+    if (targetUser.length) {
+      throw HTTPSTATUS.CONFLICT;
+    }
+    const hashedPassword = await bcrypt.hash(std_id, 10);
+    const saveUser = new User({
+      ...payload,
+      std_id: hashedPassword,
+    });
+    await saveUser.save();
+  } catch (err) {
+    throw err;
+  }
+};
