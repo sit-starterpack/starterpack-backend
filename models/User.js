485fcbacacc0ad06a15b8043273a24a88768f9e5
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-06-30 02:25:42 +0700

    [Add] mongodb model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  nickname: {
+    type: String,
+    required: true,
+  },
+  std_id: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'user',
+  },
+  feedbacks: [
+    {
+      feedbackId: {
+        type: Schema.Types.ObjectId,
+        ref: 'feedback',
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model('user', UserSchema);
