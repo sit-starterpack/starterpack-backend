d83517c876a21ff13e408e99688654eca8961479
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-07-04 15:24:24 +0700

    [Update] move dotenv config above port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const db_service = require('./services/database-service');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
@@ -9,7 +10,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 const userRoute = require('./routers/user-route');
-require('dotenv').config();
 
 db_service();
 app.use('/api/status', (req, res) => {
