ab9884047d8516c345e3a19f38593b449a3db27c
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-07-09 22:24:21 +0700

    [Update] handle error when client no authorization in req.header

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -1,7 +1,9 @@
+const { HTTPSTATUS } = require('../enumeration/httpStatus');
 const { checkRoleAdmin } = require('../services/user-service');
 async function checkRole(req, res, next) {
   const clientAuthorization = req.headers.authorization;
   try {
+    if (!clientAuthorization) throw HTTPSTATUS.UNAUTHORIZED;
     const std_id = clientAuthorization.split(' ')[1];
     const isAdmin = await checkRoleAdmin(std_id);
     if (isAdmin) next();
