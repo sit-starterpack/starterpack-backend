eb2c4e720885a9054de467f28c3866a0f3bd2dad
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-07-09 22:27:21 +0700

    [Update] change message when no permission

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -1,9 +1,12 @@
-const { HTTPSTATUS } = require('../enumeration/httpStatus');
 const { checkRoleAdmin } = require('../services/user-service');
 async function checkRole(req, res, next) {
   const clientAuthorization = req.headers.authorization;
   try {
-    if (!clientAuthorization) throw HTTPSTATUS.UNAUTHORIZED;
+    if (!clientAuthorization)
+      throw {
+        code: 401,
+        message: "You don't have permission to do this",
+      };
     const std_id = clientAuthorization.split(' ')[1];
     const isAdmin = await checkRoleAdmin(std_id);
     if (isAdmin) next();
